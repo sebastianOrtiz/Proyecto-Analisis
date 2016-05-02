326a1d47cbfe0267e19ccc5f1564299e6b319c95
sebastian_ortiz_valencia <sebastianortiz989@gmail.com>
2016-05-01 23:41:20 -0500

    Se inicia el repositorio y se elige la libreri a trabajar implementando un ejemplo

---
+++ Proyecto Analisis/build/web/index.jsp
@@ -0,0 +1,33 @@
+<%-- 
+    Document   : index
+    Created on : 1/05/2016, 10:59:43 PM
+    Author     : Sebas
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width">
+    <title> Chart example 1 </title>
+    <link rel="stylesheet" href="css/libreriaGrafico/Treant.css">
+    <link rel="stylesheet" href="css/vista/index.css">
+    
+    <link rel="stylesheet" href="css/libreriaGrafico/perfect-scrollbar.css">
+
+</head>
+<body>
+    <button name="btn1" id="btn1" >Boton grafico</button>
+    <div class="chart" id="OrganiseChart1"></div>
+    <script src="js/libreriaGrafico/raphael.js"></script>
+    <script src="js/libreriaGrafico/Treant.js"></script>
+    
+    <script src="js/libreriaGrafico/jquery.min.js"></script>
+    <script src="js/libreriaGrafico/jquery.mousewheel.js"></script>
+    <script src="js/libreriaGrafico/perfect-scrollbar.js"></script>
+    
+    <script src="js/vistas/index.js"></script>
+</body>
+</html>
---
+++ Proyecto Analisis/build/web/js/vistas/index.js
@@ -0,0 +1,131 @@
+var config = {
+        container: "#OrganiseChart1",
+        rootOrientation:  'WEST', // NORTH || EAST || WEST || SOUTH
+        // levelSeparation: 30,
+        siblingSeparation:   20,
+        subTeeSeparation:    60,
+        scrollbar: "fancy",
+        
+        connectors: {
+            type: 'step'
+        },
+        node: {
+            HTMLclass: 'nodeExample1'
+        }
+    },
+    sebas = {
+        text: {
+            name: "Mark Hill",
+            title: "Chief executive officer",
+            contact: "Tel: 01 213 123 134",
+        },
+        image: "imagenes/2.jpg",
+        HTMLid: "sebas"
+    },
+
+    cto = {
+        parent: sebas,
+        text:{
+            name: "Joe Linux",
+            title: "Chief Technology Officer",
+        },
+        stackChildren: true,
+        image: "imagenes/1.jpg",
+        HTMLid: "coo"
+    },
+    cbo = {
+        parent: sebas,
+        text:{
+            name: "Linda May",
+            title: "Chief Business Officer",
+        },
+        image: "imagenes/5.jpg",
+        HTMLid: "cbo"
+    },
+    cdo = {
+        parent: sebas,
+        text:{
+            name: "John Green",
+            title: "Chief accounting officer",
+            contact: "Tel: 01 213 123 134",
+        },
+        image: "imagenes/6.jpg",
+        HTMLid: "cdo"
+    },
+    cio = {
+        parent: cto,
+        text:{
+            name: "Ron Blomquist",
+            title: "Chief Information Security Officer"
+        },
+        image: "imagenes/8.jpg",
+        HTMLid: "cio"
+    },
+    ciso = {
+        parent: cto,
+        text:{
+            name: "Michael Rubin",
+            title: "Chief Innovation Officer",
+            contact: "we@aregreat.com"
+        },
+        image: "imagenes/9.jpg",
+        HTMLid: "ciso"
+    },
+    cio2 = {
+        parent: cdo,
+        text:{
+            name: "Erica Reel",
+            title: "Chief Customer Officer"
+        },
+        link: {
+            href: "http://www.google.com",
+            target: "_blank"
+        },
+        image: "imagenes/10.jpg",
+        HTMLid: "cio2"
+    },
+    ciso2 = {
+        parent: cbo,
+        text:{
+            name: "Alice Lopez",
+            title: "Chief Communications Officer"
+        },
+        image: "imagenes/7.jpg",
+        HTMLid: "ciso2"
+    },
+    ciso3 = {
+        parent: cbo,
+        text:{
+            name: "Mary Johnson",
+            title: "Chief Brand Officer"
+        },
+        image: "imagenes/4.jpg",
+        HTMLid: "ciso2"
+    },
+    ciso4 = {
+        parent: cbo,
+        text:{
+            name: "Kirk Douglas",
+            title: "Chief Business Development Officer"
+        },
+        image: "imagenes/11.jpg",
+        HTMLid: "ciso2"
+    }
+
+    ALTERNATIVE = [
+        config,
+        sebas,
+        cto,
+        cbo,
+        cdo,
+        cio,
+        ciso,
+        cio2,
+        ciso2,
+        ciso3,
+        ciso4
+    ];
+    
+    $("#btn1").click(function (){
+        new Treant( ALTERNATIVE );
+    });
